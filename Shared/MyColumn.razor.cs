31c914eb333d78fd5be146cbd28498f61cf9183a
ScarletKuro <artem.melnikov@live.com>
2023-05-27 14:12:37 +0300

    Improve repro

--- Shared/MyColumn.razor.cs
+++
@@ -1,6 +0,0 @@
-using MudBlazor;
-using Repro.Data;
-
-namespace Repro.Shared;
-
-public class MyColumn<TProperty> : PropertyColumn<MyViewModel, TProperty> { }
---
+++ src/Shared/MyColumn.razor.cs
@@ -0,0 +1,8 @@
+using MudBlazor;
+using Repro.Data;
+
+namespace Repro.Shared;
+
+public class MyColumn<TProperty> : PropertyColumn<MyViewModel, TProperty>
+{
+}
